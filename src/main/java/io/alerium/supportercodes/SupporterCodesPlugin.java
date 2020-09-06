40707126f54ee9f0185c1c7cd4470ffd68126a90
Frosty <frostyprime13@gmail.com>
2020-09-06 17:32:50 +0200

    Initial Setup Commit

---
+++ src/main/java/io/alerium/supportercodes/SupporterCodesPlugin.java
@@ -0,0 +1,20 @@
+package io.alerium.supportercodes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SupporterCodesPlugin extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+
+}
